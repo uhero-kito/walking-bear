c0d2e19b3fbde782cc1e6c496e61da1075841a56
uhero-kito <takayuki.kito@uhero.co.jp>
2015-03-12 15:51:37 +0900

    Install enchant.js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function main() {
+    enchant(); // initialize
+    var game = new Core(320, 320); // game stage
+    game.preload("img/chara1.png"); // preload image
+    game.fps = 20;
+
+    game.onload = function () {
+        var bear = new Sprite(32, 32);
+        bear.image = game.assets["img/chara1.png"];
+        game.rootScene.addChild(bear);
+        bear.frame = [6, 6, 7, 7];   // select sprite frame
+
+        bear.tl.moveBy(288, 0, 90)   // move right
+                .scaleTo(-1, 1, 10)  // turn left
+                .moveBy(-288, 0, 90) // move left
+                .scaleTo(1, 1, 10)   // turn right
+                .loop();             // loop it
+    };
+
+    game.start(); // start your game!
+}
