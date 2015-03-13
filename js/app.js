b1ac02ea13c24b16eed63871a34637a3919cfada
uhero-kito <takayuki.kito@uhero.co.jp>
2015-03-13 18:52:10 +0900

    Add cursor.png and arrange images

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function main() {
     enchant(); // initialize
     var game = new Core(320, 480); // game stage
-    game.preload("img/chara1.png"); // preload image
+    game.preload("img/chara1.png", "img/cursor.png"); // preload image
     game.fps = 20;
 
     game.onload = function () {
@@ -11,12 +11,19 @@ function main() {
                 var sprite = new Sprite(32, 32);
                 sprite.image = game.assets["img/chara1.png"];
                 sprite.frame = [0];
-                sprite.x = 160;
-                sprite.y = 160;
+                sprite.x = 144;
+                sprite.y = 144;
                 return sprite;
             })();
-
+            var cursor = (function () {
+                var sprite = new Sprite(160, 160);
+                sprite.image = game.assets["img/cursor.png"];
+                sprite.x = 80;
+                sprite.y = 320;
+                return sprite;
+            })();
             scene.addChild(bear);
+            scene.addChild(cursor);
             return scene;
         })();
 
